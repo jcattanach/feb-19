416fecf2a38208233157bb4de0ac2d52bcbb3a7a
Jack Cattanach <jackcattanach@live.com>
2019-02-05 14:01:44 -0600

    second commit

--- client/src/BaseLayout.js
+++ client/src/BaseLayout.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
+import Menu from './components/Menu'
+import Footer from './components/Footer'
+import './components/style/BaseLayout.css'
 
 class BaseLayout extends Component {
   render() {
     return (
-      <div>
-      Home
+      <div className="BaseLayout">
+      <Menu/>
+      <div className="heroImg">
+      Jack Cattanach
+      </div>
+      <Footer/>
       </div>
     );
   }
---
+++ client/src/components/Menu.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './style/Menu.css';
+
+const Menu = () => {
+  return(
+  <div className="Menu">Menu</div>
+  )
+}
+
+export default Menu
